0597172acd9797e24da7e83084d4a63820aea157
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2020-10-24 22:20:14 +0200

    dm functionality

---
+++ dmIdiots.py
@@ -0,0 +1,55 @@
+import time
+import csv
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+users = ['scarra', 'scarra']
+browser = webdriver.Chrome('H:\downloads\chromedriver_win32\chromedriver.exe')
+userId = []
+text = 'I sent this using my bot..., its so fucking late... crack crack crack, i want to die'
+
+def login(bowser): 
+	browser.get('https://twitter.com/login')
+	time.sleep(1)
+	username = browser.find_element_by_css_selector("input[name='session[username_or_email]']")
+	password= browser.find_element_by_css_selector("input[name='session[password]']")
+	username.send_keys("suijkerbuijk_j")
+	password.send_keys("jessiefag123")
+
+	submit = browser.find_element_by_xpath("//div[@data-testid='LoginForm_Login_Button']")
+	submit.click()
+
+
+def get_user_ids(browser):
+	# print(browser.find_element_by_xpath("//div[@data-testid='392513215-unfollow']"))
+	time.sleep(1)
+	userId = browser.find_element_by_xpath("//div[contains(@data-testid, 'follow')]")
+	splitString = userId.get_attribute("data-testid").split("-", 1)
+	messages(splitString[0])
+
+def get_users():
+	# with open("idiotScraper_users_csv", 'r', errors='ignore') as file:
+	#     reader = csv.reader(file, delimiter=',', quotechar='|')
+	#     for row in reader:
+	#         print(row)
+	for user in users:
+		browser.get('https://twitter.com/' + user)
+		time.sleep(1)
+		userId = get_user_ids(browser)
+
+def messages(userId):
+	browser.get('https://twitter.com/messages/' + userId + '-1229336829465415680?text=' + text)
+	time.sleep(1)
+	sent = browser.find_element_by_xpath("//div[@data-testid='dmComposerSendButton']")
+	browser.execute_script("arguments[0].click();", sent)
+
+	#Check if i can open browser with cookies so my twitter stays logged in
+	# if this is not possible make sure we log in using python
+
+
+
+        
+time.sleep(1)
+login(browser)
+users = get_users()
--- getIdiots.py
+++ getIdiots.py
@@ -7,18 +7,18 @@ def get_tweets(browser):
     body = browser.find_element_by_tag_name('body')
     tweets = []
     while len(tweets) < num_of_tweets:
-        for _ in range(5):
+        for _ in range(1):
             body.send_keys(Keys.PAGE_DOWN)
             time.sleep(1)
         # tweets = browser.find_elements_by_xpath("//div[@aria-label='Tijdlijn: Tijdlijn doorzoeken']")
-        tweets = browser.find_elements_by_xpath("//div[@data-testid='tweet']")
+        tweets += browser.find_elements_by_xpath("//div[@data-testid='tweet']")
     return tweets
 
 def parse_and_save(tweets, num_of_tweets, query):
     counter = 0
     column_separator = ","
 
-    with open("idiotScraper_users_csv", 'w', errors='ignore') as file:
+    with open("idiotScraper_users_csv", 'w', newline='', errors='ignore') as file:
         fieldnames = ['User:']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
@@ -28,14 +28,14 @@ def parse_and_save(tweets, num_of_tweets, query):
                 break
             user = tweet.find_element_by_class_name('css-901oao').text
 
-            writer.writerow({'User': user})
+            writer.writerow({'User:': user})
             counter += 1
 
 browser = webdriver.Chrome('H:\downloads\chromedriver_win32\chromedriver.exe');
 base_url = "https://twitter.com/search?q="
 query = '%23twitch'
 url = base_url + query
-num_of_tweets = 5;
+num_of_tweets = 25;
 
 browser.get(url)
 time.sleep(1)
