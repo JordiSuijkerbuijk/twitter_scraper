2d0a6661efc7671c4c1db228925069b1cc6aef13
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2020-11-25 00:13:18 +0100

    Fully works now, Added suggestions to dmIdiots for future

--- dmIdiots.py
+++ dmIdiots.py
@@ -7,48 +7,49 @@ from selenium.webdriver.common.keys import Keys
 users = ['scarra', 'scarra']
 browser = webdriver.Chrome('H:\downloads\chromedriver_win32\chromedriver.exe')
 userId = []
-text = 'I sent this using my bot..., its so fucking late... crack crack crack, i want to die'
+text = 'Hey, im working on an expirement to find out what the best way is to create a following for a twitch channel using both social media and bots. If u are interested in gaming or programming i would appreciate it if u would follow my channel: twitch.tv/zolderraam'
 
 def login(bowser): 
-	browser.get('https://twitter.com/login')
-	time.sleep(1)
-	username = browser.find_element_by_css_selector("input[name='session[username_or_email]']")
-	password= browser.find_element_by_css_selector("input[name='session[password]']")
-	username.send_keys("input username")
-	password.send_keys("input password")
+    browser.get('https://twitter.com/login')
+    time.sleep(1)
+    username = browser.find_element_by_css_selector("input[name='session[username_or_email]']")
+    password= browser.find_element_by_css_selector("input[name='session[password]']")
+    username.send_keys("zolderraam1")
+    password.send_keys("Flameislame123")
 
-	submit = browser.find_element_by_xpath("//div[@data-testid='LoginForm_Login_Button']")
-	submit.click()
+    submit = browser.find_element_by_xpath("//div[@data-testid='LoginForm_Login_Button']")
+    submit.click()
 
 
 def get_user_ids(browser):
-	# print(browser.find_element_by_xpath("//div[@data-testid='392513215-unfollow']"))
-	time.sleep(1)
-	userId = browser.find_element_by_xpath("//div[contains(@data-testid, 'follow')]")
-	splitString = userId.get_attribute("data-testid").split("-", 1)
-	messages(splitString[0])
+    # print(browser.find_element_by_xpath("//div[@data-testid='392513215-unfollow']"))
+    time.sleep(1)
+    userId = browser.find_element_by_xpath("//div[contains(@data-testid, 'follow')]")
+    splitString = userId.get_attribute("data-testid").split("-", 1)
+    messages(splitString[0])
 
 def get_users():
-	# with open("idiotScraper_users_csv", 'r', errors='ignore') as file:
-	#     reader = csv.reader(file, delimiter=',', quotechar='|')
-	#     for row in reader:
-	#         print(row)
-	for user in users:
-		browser.get('https://twitter.com/' + user)
-		time.sleep(1)
-		userId = get_user_ids(browser)
+    with open("idiotScraper_users_csv", 'r', errors='ignore') as file:
+        reader = csv.reader(file, delimiter=',', quotechar='|')
+        next(reader)
+    #     for row in reader:
+    #         print(row)
+        for user in reader:
+            browser.get('https://twitter.com/' + user[0])
+            time.sleep(1)
+            userId = get_user_ids(browser)
 
 def messages(userId):
-	browser.get('https://twitter.com/messages/' + userId + '-1229336829465415680?text=' + text)
-	time.sleep(1)
-	sent = browser.find_element_by_xpath("//div[@data-testid='dmComposerSendButton']")
-	browser.execute_script("arguments[0].click();", sent)
+    browser.get('https://twitter.com/messages/' + userId + '-1330631224826748932?text=' + text)
+    time.sleep(1)
+    try:
+        sent = browser.find_element_by_xpath("//div[@data-testid='dmComposerSendButton']")
+        browser.execute_script("arguments[0].click();", sent)
+        return True
+    except:
+        return False
 
-	#Check if i can open browser with cookies so my twitter stays logged in
-	# if this is not possible make sure we log in using python
-
-
-
+    #Check if i can open browser with cookies so my twitter stays logged in
         
 time.sleep(1)
 login(browser)
--- getIdiots.py
+++ getIdiots.py
@@ -3,18 +3,27 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+#get the @'s instead of
+
 def get_tweets(browser): 
     body = browser.find_element_by_tag_name('body')
-    tweets = []
-    while len(tweets) < num_of_tweets:
+    userList = []
+    while len(userList) < num_of_tweets:
         for _ in range(1):
             body.send_keys(Keys.PAGE_DOWN)
             time.sleep(1)
-        # tweets = browser.find_elements_by_xpath("//div[@aria-label='Tijdlijn: Tijdlijn doorzoeken']")
-        tweets += browser.find_elements_by_xpath("//div[@data-testid='tweet']")
-    return tweets
+        # tweets += browser.find_elements_by_xpath("//div[@data-testid='tweet']")
 
-def parse_and_save(tweets, num_of_tweets, query):
+        user = browser.find_element_by_class_name('css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-1f6r7vd').text
+        
+        if user not in userList:
+            userList.append(user)
+
+        print(userList)
+
+    return userList
+
+def parse_and_save(userList, num_of_tweets, query):
     counter = 0
     column_separator = ","
 
@@ -23,17 +32,13 @@ def parse_and_save(tweets, num_of_tweets, query):
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
 
-        for tweet in tweets:
-            if counter == num_of_tweets:
-                break
-            user = tweet.find_element_by_class_name('css-901oao').text
-
+        for user in userList:
             writer.writerow({'User:': user})
             counter += 1
 
 browser = webdriver.Chrome('H:\downloads\chromedriver_win32\chromedriver.exe');
 base_url = "https://twitter.com/search?q="
-query = '%23twitch'
+query = '%23tft'
 url = base_url + query
 num_of_tweets = 25;
 
